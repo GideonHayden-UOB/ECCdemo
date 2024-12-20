2b2fc89f918024173811f68bb62730d40a256347
Gideon Hayden <157599633+GideonHayden-UOB@users.noreply.github.com>
2024-12-20 19:28:40 +0000

    added point negation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt 
+from dataclasses import dataclass
 
 
 class EllipticCurve:
@@ -18,7 +19,7 @@ class EllipticCurve:
         return (np.sqrt(self.y2Value(x)),-np.sqrt(self.y2Value(x)))
     
 class FiniteFieldEllipticCurve:
-    def __init__(self, a, b, p):
+    def __init__(self, a, b, p:int):
         self.a = a
         self.b = b
         self.p = p
@@ -34,9 +35,20 @@ class FiniteFieldEllipticCurve:
     
     def isElem(self,x,y):
         return ((x**3 + self.a*x + self.b - y**2)%self.p ==0)
-    
 
-    
+@dataclass
+class Coordinate:
+    x: int
+    y: int  
+
+Point = str | Coordinate
+
+def pointNegation(curve:EllipticCurve, point:Point):
+    match point:
+        case str(string):
+            return "infinity"
+        case Coordinate(x,y):
+            return Coordinate(x,-y)
 
 curve = EllipticCurve(-2,4)
 print(curve.y2Value(2))
@@ -46,3 +58,6 @@ print(pow(0.5,2))
 curve2 = FiniteFieldEllipticCurve(0,7,17)
 print(curve2.isElem(9,15))
 print(curve2.isElem(5,8))
+
+coord = Coordinate(3,4)
+print(pointNegation(curve, Coordinate(3,4)))
